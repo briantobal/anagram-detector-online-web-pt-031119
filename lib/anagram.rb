44a6e50b4e829930896396095746f7e24a9971dd
Brian Tobal <brian.tobal@gmail.com>
2019-04-11 18:02:50 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,8 +7,8 @@ class Anagram
   end
   
   def match(word_array)       #takes an array of words         
-    word_array.slice(0,100)       #split the words into arrays
-     binding.pry                          #split the arrays into letters
+    word_array.select { }       #split the words into arrays
+                            #split the arrays into letters
                               #sort the arrays 
   end                              #compare the arrays 
   
