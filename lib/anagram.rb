1f169b75f3096d79a9567e69a4fa0bd3e948515e
Brian Tobal <brian.tobal@gmail.com>
2019-04-11 17:41:07 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,16 @@
 class Anagram 
+  attr_accessor :word  
   
-  def initialize :word 
+  def initialize (word) 
+    word = @word 
   end
   
   def match(word_array)   #takes an array of ana
-    word_array =                
+    #word_array               
     
     
     #compare word w/ word_array                 #return all matches in [ ] or if none an empty [ ]
   end 
   
-  attr_accessor
+  
 end 
