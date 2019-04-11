0615ab86fd2fda991ab8c6086144a9e40f9e33b0
Brian Tobal <brian.tobal@gmail.com>
2019-04-11 18:03:59 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,13 +7,11 @@ class Anagram
   end
   
   def match(word_array)               
-    word_array.select {|x|x.sort==@word.sort }       #split the words into arrays
-                            #split the arrays into letters
-                              #sort the arrays 
-  end                              #compare the arrays 
-  
-  
-  
+    word_array.select {|x|x.sort==@word.sort }
+                        
+  end                              
+
+
   
   
   # array.select {|x| x.split("").sort == @name.split("").sort}
