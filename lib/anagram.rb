555db6b11d7db26c2015c429915582117309cec3
Brian Tobal <brian.tobal@gmail.com>
2019-04-11 18:05:26 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
   end
   
   def match(word_array)               
-    word_array.select {|x|x.sort==@word.sort }
+    word_array.select {|x|x.split==@word.sort }
                         
   end                              
 
